cee1c543c06fa894eda7216933d89131c167369f
Casper Winsnes <cwinsnes92@gmail.com>
2020-11-28 02:01:36 +0100

    Switch from GLUT to GLFW.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,39 @@
 #include "tile.hpp"
-#include <GL/freeglut.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-
-//Program to create an empty Widdow
-void init(){
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);    //Line C
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(1000,200);
-    glutCreateWindow("Simple Window");
-}
-
-void display()
-{
-    glClearColor(1.0,1.0,1.0,0.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    // gluOrtho2D(0.0,100.0,0,100.0);
-    glFlush();
-}
 
 int main(int argc,char **argv)
 {
-    glutInit(&argc,argv);            //Line A
-    init();                    //Line B
-    glutDisplayFunc(display);
-    glutMainLoop();
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }

854c78ffcd729d1f7857eeacce0d9e1c5b095957
Casper Winsnes <cwinsnes92@gmail.com>
2020-11-28 01:14:25 +0100

    change tabs to spaces

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,25 @@
 
 //Program to create an empty Widdow
 void init(){
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);	//Line C
-	glutInitWindowSize(640,480);
-	glutInitWindowPosition(1000,200);
-	glutCreateWindow("Simple Window");
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);    //Line C
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(1000,200);
+    glutCreateWindow("Simple Window");
 }
 
 void display()
 {
-	glClearColor(1.0,1.0,1.0,0.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	// gluOrtho2D(0.0,100.0,0,100.0);
-	glFlush();
+    glClearColor(1.0,1.0,1.0,0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    // gluOrtho2D(0.0,100.0,0,100.0);
+    glFlush();
 }
 
 int main(int argc,char **argv)
 {
-	glutInit(&argc,argv);			//Line A
-	init();					//Line B
-	glutDisplayFunc(display);
-	glutMainLoop();
-	return 0;
+    glutInit(&argc,argv);            //Line A
+    init();                    //Line B
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
 }
